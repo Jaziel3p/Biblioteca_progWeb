899fecc217ce8aa948e43d17618d55853b4d7a83
Manuel Apolinar Castillo <apolinar121070@gmail.com>
2022-11-29 01:21:23 -0600

    estructura MVC del proyecto

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1d299e39ee0838e52624fbb8b2605f8aac139819',
+        'name' => 'programacion_web/biblioteca',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'programacion_web/biblioteca' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1d299e39ee0838e52624fbb8b2605f8aac139819',
+            'dev_requirement' => false,
+        ),
+    ),
+);
